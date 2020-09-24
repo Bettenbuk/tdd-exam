05a3c15d685c22928b522046bdc53c024ef9fbdf
Bettenbuk István <istvan.bettenbuk@idomsoft.hu>
2020-09-24 11:19:35 +0200

    Metro ticket time validation is ready

--- src/test/java/org/bi/MetroTicketValidatorTest.java
+++ src/test/java/org/bi/MetroTicketValidatorTest.java
@@ -7,35 +7,35 @@ import java.time.LocalDateTime;
 public class MetroTicketValidatorTest {
 
     private static final int VALIDATION_TIME = 80;
-    private MetroTicketValidator metroTicketValidator = new MetroTicketValidator();
+    private final MetroTicketValidator metroTicketValidator = new MetroTicketValidator();
     private LocalDateTime ticketUsingStartTime;
-    private String ticket;
+    private String ticket = "0643xxx";
 
     @Test
     public void ticketIsExpired(){
         ticketUsingStartTime= LocalDateTime.now().minusMinutes(VALIDATION_TIME+1);
-        ticket = "0643xxx"+TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
+        ticket += TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
         Assert.assertFalse(metroTicketValidator.isValid(ticket));
     }
 
     @Test
     public void ticketFromPast(){
         ticketUsingStartTime = LocalDateTime.now().plusMinutes(1);
-        ticket = "0643xxx"+TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
+        ticket += TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
         Assert.assertFalse(metroTicketValidator.isValid(ticket));
     }
 
     @Test
     public void validTicketInCaseLastMinute(){
         ticketUsingStartTime = LocalDateTime.now().minusMinutes(VALIDATION_TIME - 1);
-        ticket = "0643xxx"+TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
+        ticket += TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
         Assert.assertTrue(metroTicketValidator.isValid(ticket));
     }
 
     @Test
     public void validTicketInCaseImmediately(){
         ticketUsingStartTime = LocalDateTime.now().minusMinutes(0);
-        ticket = "0643xxx"+TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
+        ticket += TimeConverter.convertToMetroTimeFormat(ticketUsingStartTime);
         Assert.assertTrue(metroTicketValidator.isValid(ticket));
     }
 }
